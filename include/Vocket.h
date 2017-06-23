ad322a007bb0aa56643edf1b5254145adaf574e6
WhyWhy <daxiaonewwhy@163.com>
2017-06-23 11:48:03 +0800

    Add architecture doc which indicates how to finish this project

---
+++ include/Vocket.h
@@ -0,0 +1,26 @@
+/*///////
+
+This is the main class Vocket.
+You can use this library just include this header file and get a instance of Vocket class.
+I will use OO method, which will be more clearly and beautiful I think.
+
+*////////
+
+#ifndef VOCKET_H
+#define VOCKET_H
+
+class Vocket {
+	// The private variable.
+	private:
+
+	// The public variable.
+	public:
+
+	// The private function.
+	private:
+
+	// The public function.
+	public:
+};
+
+#endif
